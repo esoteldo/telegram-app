9ee6b310475a39ac32d930662e8afbfaef62ae2f
esoteldo <soteldo18@hotmail.com>
2025-08-01 09:35:52 -0400

    test-9:35-1-8-2025

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,9 @@ function App() {
     
       <Suspense fallback={<div>Loading...</div>}>
       {/* BrowserRouter wraps the entire app to enable routing */}
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
     <div className="container">
+      <h1>{import.meta.url}</h1>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/buytickets" element={<BuyTickets />} />
